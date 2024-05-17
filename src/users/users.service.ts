3e66e4029b6a976617116316c65ab6ca5a2e65c8
Fazla Rabbi <fazlarabbi76@gmail.com>
2024-05-17 20:53:14 +0600

    user CRUD done

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const hash = await bcrypt.hash(createUserDto.password, 10);
+    createUserDto.password = hash;
+    return this.userRepository.save(createUserDto).catch((e) => {
+      // console.log(e)
+      if (e.code == 'ER_DUP_ENTRY') {
+        return {
+          statusCode: 'DUPLICATE_VALUE',
+          message: e.sqlMessage,
+        };
+      } else {
+        console.log(e);
+        return {
+          statusCode: 'UNKNOWN_ERROR',
+          details: e,
+        };
+      }
+    });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    return await this.getUserInfo(id, true);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      const hash = await bcrypt.hash(updateUserDto.password, 10);
+      updateUserDto.password = hash;
+    }
+    await this.userRepository.update(id, updateUserDto);
+
+    return await this.getUserInfo(id);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.userRepository.delete(id);
+  }
+
+  async getUserInfo(id: number, rel: boolean = false): Promise<User> {
+    let relation = [];
+    if (rel) {
+      relation = [];
+    }
+
+    const userInfo: User = await this.userRepository.findOne({
+      where: {
+        id: id,
+      },
+      relations: relation,
+    });
+
+    if (userInfo) {
+      return userInfo;
+    }
+
+    throw new HttpException('User  not found', HttpStatus.NOT_FOUND);
   }
 }
